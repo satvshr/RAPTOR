fc9a9439e30795d06f5201ad819a851a35f57af4
satvshr <satvmishi@gmail.com>
2024-12-11 21:24:25 +0530

    Almost done with maximization logic, got to fix cov bugs

--- utils/gmm.py
+++ utils/gmm.py
@@ -8,17 +8,17 @@ def gmm(documents, n_classes):
     print("doc: ", documents.shape)
     # All pi's should add up to 1
     random_numbers = np.random.rand(n_classes)
-    pi = random_numbers / np.sum(random_numbers)
-
+    pi = np.array(random_numbers / np.sum(random_numbers))
+    print("pi", pi.shape)
     # Initialize means by randomly choosing data points
-    mean = random.sample(documents.tolist(), k=n_classes)  # Convert documents to list
-    print("mean: ", np.array(mean).shape)
+    mean = np.array(random.sample(documents.tolist(), k=n_classes))  # Convert documents to list
+    print("mean: ", mean.shape)
 
     # Initialize covariance matrices by assigning each one as an identity matrix of size (classes, dim, dim)
     cov  = np.tile(np.identity(dim), (n_classes, 1, 1))
     print("cov: ", cov.shape)
     # Compute responsibilities
-    def expectation(n_classes, pi, mean, cov):
+    def expectation(pi, mean, cov):
         def get_gaussian_sum(gaussian):
             gaussian_sums = np.zeros([size]) # Sum of gaussian for (all)point j across all i classes
             for i in range(size):
@@ -38,21 +38,41 @@ def gmm(documents, n_classes):
 
             for j in range(size): # To indicate each data point
                 dif[i, j] = np.array(documents[j] - mean[i])
-                print("dif: ", dif.shape)
+                # print("dif: ", dif.shape)
                 mahalanobis[i, j] = np.dot(np.dot(dif[i, j].T, np.linalg.inv(cov[i])), dif[i, j])
                 exp[i, j] = np.exp(-1/2 * mahalanobis[i, j])
 
                 N[i, j] = normalization_constant * exp[i, j]
                 gaussian[i, j] = pi[i] * N[i, j]
-        print("gaussian", gaussian)
+        # print("gaussian", gaussian)
         gaussian_sums = get_gaussian_sum(gaussian)
-        print(gaussian_sums)
+        # print(gaussian_sums)
         for i in range(n_classes):
             for j in range(size):
                 responsibility[i, j] = gaussian[i, j] / gaussian_sums[j]
 
-        print(responsibility)
-        return responsibility
-    expectation(n_classes, pi, mean, cov)
+        # print(responsibility)
+        return responsibility, dif
+    responsiblity, dif = expectation(pi, mean, cov)
+    print(responsiblity)
+
+    def maximization(responsibilities, dif):
+        new_pi = np.empty_like(pi, dtype=float)
+        new_mean = np.empty_like(mean, dtype=float)
+        new_cov = np.empty_like(cov, dtype=float)
+
+        for i in range(n_classes):
+            resp_sum = np.sum(responsibilities[i])
+            new_pi[i] = resp_sum / size
+            print(dif[1].shape)
+            print("dot", [np.dot((responsibilities[i, j] * dif[i]), dif[i].T) for j in range(size)])
+            new_mean[i] = np.sum(np.vstack([responsibilities[i, j] * documents[j] for j in range(size)]), axis=0) / resp_sum
+            new_cov[i] = np.sum(np.vstack([np.dot((responsibilities[i, j] * dif[i, j]), dif[i, j].T) for j in range(size)]), axis=0) / resp_sum
+
+        return new_pi, new_mean, new_cov
+    pi, mean, cov = maximization(responsiblity, dif)
+    print("pi", pi)
+    print("mean", mean)
+    print("cov", cov)
 
 gmm(np.array([[1, 2],[2, 3],[3, 4],[4, 6]]), 3)
