d140c88c5d5b42959ccf13a01c02e73630ee3c85
satvshr <satvmishi@gmail.com>
2024-12-17 14:46:37 +0530

    Realised original gmm had no issues, input was too small to get a good output hence was getting errors

--- src/raptor.py
+++ src/raptor.py
@@ -1,6 +1,6 @@
 from langsmith import Client as traceable
 from utils.gmm import gmm
-
+from utils.umap import umap
 k_global = 30
 k_local = 5
 
@@ -8,7 +8,8 @@ k_local = 5
 def raptor_template():
     def raptor(doc_splits, embedder, top_k):
         print(len(doc_splits))
-        # umap_1 = gmm(doc_splits, 
+        umap_1 = umap(doc_splits, embedder, 17, 10)
+        gmm(umap_1, 3)
         return doc_splits
         
     return raptor
