cf8d375f545266182c50af7a9f4445a251609333
satvshr <satvmishi@gmail.com>
2024-12-24 01:53:20 +0530

    Integrated everything together into 1 chain and renamed exp.py to main.py

--- src/main.py
+++ src/main.py
@@ -1,56 +1,102 @@
 from dotenv import load_dotenv
 from langchain.schema.runnable import RunnableSequence
 from utils.lm_studio import LMStudioLLM
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain.schema.runnable import RunnableLambda
 from src.translation import translation_template
-from src.routing import routing_template
+from src.routing import semantic_routing_template
 from src.indexing import indexing_template
+from src.raptor import raptor_template
 from src.retrieval import retrieval_template
 from src.generation import generation_template
 
 from utils.pdf_summarizer import get_summaries
 
+# Load environment variables
 load_dotenv()
 
+# Initialize LLM, number of splits to retrieve, text splitter, and the embedder
 lm_studio_llm = LMStudioLLM(path='completions')
-
-llm_chain_file = RunnableSequence(
-    translation_template() | 
-    lm_studio_llm |
-    (lambda translation_output: {
-        'routing_output': routing_template().format(
-            question=question['question'],  
-            file_summaries=file_summaries  
-        ),
-        'translation_output': translation_output
-    }) | 
-    lm_studio_llm | 
-    (lambda outputs: indexing_template().format(
-        routing_output=outputs['routing_output'], 
-        translation_output=outputs['translation_output']
-    )) | 
-    lm_studio_llm
-    # retrieval_template() | 
-    # lm_studio_llm | 
-    # generation_template()
+top_k_indexing = 40
+top_k_raptor = 5
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size = 300,
+    chunk_overlap = 50
 )
+embedder = GPT4AllEmbeddings()
 
-llm_chain_no_file = RunnableSequence(
-    translation_template() | lm_studio_llm
-)
+# Input question
+question = {'question': "What is twitter sentiment analysis?"}
 
-question = {'question': input("Enter q: ")}
-files = []
+# Precompute translation_output
+translation_result = translation_template() | lm_studio_llm
+translation_output = translation_result.invoke(question)
 
-for i in range(int(input("Enter the number of files: "))):
-    files.append(input("Enter file name: "))
-        
-if len(files) == 0:
-    answer = llm_chain_no_file.invoke(question)
-else:
+# Gather files
+files = ["1", "2", "3"]
+# for i in range(int(input("Enter the number of files: "))):
+#     files.append(input("Enter file name: "))
+
+# Generate file summaries
+file_summaries = ""
+if len(files) > 0:
     # summaries in the format [[file_name, summary], ..]
     summaries = get_summaries(files)
     file_summaries = "\n".join([f"{file}: {summary}" for file, summary in summaries])
 
+# Define llm_chain for file input
+llm_chain_file = RunnableSequence(
+    # Logical routing
+    # RunnableLambda(lambda question: logical_routing_template().format(
+    #     question=question['question'],  
+    #     file_summaries=file_summaries  
+    # )) | 
+    # lm_studio_llm |
+
+    # Semantic routing
+    RunnableLambda(lambda _: semantic_routing_template()(
+        questions=translation_output,
+        file_summaries=summaries,
+        embedder=embedder
+    )) |
+
+    # Indexing
+    RunnableLambda(lambda doc_name_list: indexing_template()(
+        documents=doc_name_list,
+        questions=translation_output,
+        text_splitter=text_splitter,
+        embedder=embedder,
+        top_k=top_k_indexing
+    )) |
+
+    # Raptor retrieval
+    RunnableLambda(lambda splits_list: raptor_template()(
+        doc_splits=splits_list,
+        questions=translation_output,
+        embedder=embedder
+    )) |
+    RunnableLambda(lambda best_cluster_nodes: retrieval_template()(
+        best_cluster_nodes=best_cluster_nodes,
+        questions=translation_output,
+        embedder=embedder
+    )) |
+    (lambda best_nodes: generation_template().format(
+        question=question['question'],
+        best_nodes=best_nodes
+    )) |
+    lm_studio_llm
+)
+
+# Define llm_chain for no file input
+llm_chain_no_file = RunnableSequence(
+    translation_template() | lm_studio_llm
+)
+
+# Execute chain based on files
+if len(files) == 0:
+    answer = llm_chain_no_file.invoke(question)
+else:
     answer = llm_chain_file.invoke({
         'question': question,
         'file_summaries': file_summaries
